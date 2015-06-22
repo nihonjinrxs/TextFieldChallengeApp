4a7f4d53f5a65cde52eecd9791265cee9445e9bf
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2015-06-21 22:34:55 -0400

    Initial Commit

---
+++ TextFieldsChallengeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldsChallengeApp
+//
+//  Created by Ryan Harvey on 6/21/15.
+//  Copyright © 2015 datascientist.guru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TextFieldsChallengeAppUITests/TextFieldsChallengeAppUITests.swift
@@ -0,0 +1,35 @@
+//
+//  TextFieldsChallengeAppUITests.swift
+//  TextFieldsChallengeAppUITests
+//
+//  Created by Ryan Harvey on 6/21/15.
+//  Copyright © 2015 datascientist.guru. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class TextFieldsChallengeAppUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
